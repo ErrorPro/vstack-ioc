ad3d7dcaed9bc171ddc9072562a23617ce9cddd0
Ven <mohtaiiihuk@gmail.com>
2015-06-20 23:47:43 +0400

    Tests rewrite with ES6

--- test/createContainer-test.js
+++ test/createContainer-test.js
@@ -1,121 +1,107 @@
 const createContainer = require('../lib/createContainer')
 const sinon = require('sinon')
 
-describe('createContainer', function() {
+describe('createContainer', () => {
   let c
 
-  beforeEach(function() {
+  beforeEach(() => {
     c = createContainer()
   })
 
-  it('should store and resolve values', function() {
+  it('should store and resolve values',() => {
     c.set('a', 'b')
 
-    return c.get('a').then(function(value) {
+    return c.get('a').then((value) => {
       assert(value, 'b')
     })
   })
 
 
-  it('should store and resolve services', function() {
-    c.set('a', function() {
-      return 'b'
-    })
+  it('should store and resolve services', () => {
+    c.set('a', () => 'b')
 
-    return c.get('a').then(function(value) {
+    return c.get('a').then((value) => {
       assert(value, 'b')
     })
   })
 
 
-  it('should resolve dependencies', function() {
-    c.set('a', function() {
-      return 1
-    })
+  it('should resolve dependencies', () => {
+    c.set('a', () => 1)
+    c.set('b', ['a'], (a) => a + 2)
 
-    c.set('b', ['a'], function(a) {
-      return a + 2
-    })
-
-    return c.get('b').then(function(value) {
+    return c.get('b').then((value) => {
       assert(value, 3)
     })
   })
 
-  it('should search services', function() {
-    c.set('a', [], ['x'], function() {
-      return 1
-    })
+  it('should search services', () => {
+    c.set('a', [], ['x'], () => 1)
+    c.set('b', [], ['x'], () => 2)
+    c.set('c', [], ['y'], () => 3)
 
-    c.set('b', [], ['x'], function() {
-      return 2
-    })
-
-    c.set('c', [], ['y'], function() {
-      return 3
-    })
-
-    return c.search('x').then(function(services) {
+    return c.search('x').then((services) => {
       assert(services, [1, 2])
     })
   })
 
 
-  it('should call plugin', function() {
+  it('should call plugin', () => {
     var plugin = sinon.spy();
+
     c.plugin(plugin)
     assert(plugin.called, true)
   })
 
-  it('should compile', function() {
+  it('should compile', () => {
     var list = []
 
-    c.plugin(function(c) {
-      c.set('lister', function() {
+    c.plugin((c) => {
+      c.set('lister', () => {
         return {
-          add: function(item) {
+          add: (item) => {
             list.push(item)
           }
         }
       })
 
-      c.compile(function() {
+      c.compile(() => {
         return Promise.all([c.get('lister'), c.search('listable')])
-          .then(function(values) {
+          .then((values) => {
             var lister = values[0]
             var listable = values[1]
 
-            listable.forEach(function(item) {
+            listable.forEach((item) => {
               lister.add(item)
             })
           })
       })
     })
 
-    c.plugin(function(c) {
+    c.plugin((c) => {
       c.set('a', [], ['listable'], 1)
       c.set('b', [], ['listable'], 2)
       c.set('c', [], ['not-listable'], 3)
     })
 
-    return c.build().then(function() {
+    return c.build().then(() => {
       assert(list, [1, 2])
     })
   })
 
 
-  it('should not rewrite service', function() {
-    assert.throws(function() {
+  it('should not rewrite service', () => {
+    assert.throws(() => {
       c.set('a', 1)
       c.set('a', 2)
     }, 'Invariant Violation: Service "a" already defined')
   })
 
-  it('should check cirrural dependency', function() {
-    assert.throws(function() {
-      c.set('a', ['b'], function() {})
-      c.set('b', ['c'], function() {})
-      c.set('c', ['a'], function() {})
+  it('should check cirrural dependency', () => {
+    assert.throws(() => {
+      c.set('a', ['b'], () => {})
+      c.set('b', ['c'], () => {})
+      c.set('c', ['a'], () => {})
     }, 'Invariant Violation: Circular dependency between "c" and "a"')
   })
 })
